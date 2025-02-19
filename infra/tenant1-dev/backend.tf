528a669e77727bca9cbf1fb06dd9a49454a5d8f0
Christoph Wegener <cwegener@users.noreply.github.com>
2025-02-20 08:44:34 +1100

    fix: terraform state missing resource group name
    │ Error: Either an Access Key / SAS Token or the Resource Group for the Storage Account must be specified - or Azure AD Authentication must be enabled

--- infra/tenant1-dev/backend.tf
+++ infra/tenant1-dev/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
+    storage_account_name = "" # Github Action Secret: `TF_STATE_STORAGE_ACCOUNT`
+    resource_group_name  = "" # Github Action Variable: `TF_STATE_RESOURCE_GROUP_NAME`
     container_name       = "gha-test"
     key                  = "terraform.tfstate"
   }
