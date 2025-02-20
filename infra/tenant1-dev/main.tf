fa5ebe3f672b22015c00d6b209b6e1053681d823
Christoph Wegener <cwegener@users.noreply.github.com>
2025-02-20 13:15:13 +1100

    feat: ugly commit

--- infra/tenant1-dev/main.tf
+++ infra/tenant1-dev/main.tf
@@ -6,6 +6,10 @@ variable "provider_token" {
   type = string
 }
 
+variable = "unused" {
+  default         = "slkgjlkfdg"      
+}
+
 module "vpc" {
   # tflint-ignore: terraform_module_pinned_source
   source         = "github.com/cwegener/gha-test.git//modules/vpc?ref=master"
