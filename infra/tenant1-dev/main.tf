4bc4a5c062c2dae1b7f409ff49af47746add7bd5
Christoph Wegener <cwegener@users.noreply.github.com>
2025-02-27 14:46:51 +1100

    feat(tf): helm release

--- infra/tenant1-dev/main.tf
+++ infra/tenant1-dev/main.tf
@@ -1,17 +1,26 @@
 terraform {
   required_version = ">= 1.10.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
 }
 
 variable "provider_token" {
   type = string
 }
 
-variable = "unused" {
-  default         = "slkgjlkfdg"      
-}
-
 module "vpc" {
   # tflint-ignore: terraform_module_pinned_source
   source         = "github.com/cwegener/gha-test.git//modules/vpc?ref=master"
   provider_token = var.provider_token
+}
+
+resource "helm_release" "example" {
+  name       = "my-redis-release"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "redis"
+  version    = "7.4.2"
 }
